4d32fd87ce8f9339c3822a91e17b672e00e60595
Izabela <izabelac163@gmail.com>
2020-02-29 17:16:53 +0100

    get started

---
+++ js/fruitClickHandler.js
@@ -0,0 +1,5 @@
+// Fruit Click Handler
+
+// first u click on a fruit
+// we need the fruit div to listen to the click
+// then we need to give it a function containing what action to take once click is detected
